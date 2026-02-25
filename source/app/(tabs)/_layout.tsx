8b3dc7661ff6e9a6c45f6bbcca12d99d35227ee4
Aleeg0 <Sankafoxe@gmail.com>
2026-02-25 08:04:19 +0300

    rewrite tabs to native + fix file name

--- source/app/(tabs)/_layout.tsx
+++ source/app/(tabs)/_layout.tsx
@@ -1,17 +1,21 @@
-import { Tabs } from "expo-router";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 
 const TabsLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "home",
-        }}
-      />
-      <Tabs.Screen name="sheduler" />
-      <Tabs.Screen name="settings" />
-    </Tabs>
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>Tracker</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="checklist" md="playlist_add_check" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="scheduler">
+        <NativeTabs.Trigger.Label>Scheduler</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="calendar.and.person" md="event" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="settings">
+        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="gear" md="settings" />
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 };
 
